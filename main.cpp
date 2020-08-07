8f57ddec481a09a2d7877a3a09584d5f7b13dacc
Makar Baderko <makarbaderko@gmail.com>
2020-08-07 20:00:47 +0300

    Added all needed features (not working)

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 #include <vector>
 
 using namespace std;
-//Print user menu
+//Print user menu +
 void user_menu(){
     string menu = "Подсказки: введите\nADD - чтобы добавить задачу\nDEL - чтобы удалить задачу\nCHG - чтобы изменить задачу";
     cout << menu;
 }
-//Print all tasks from vector
+//Print all tasks from vector +
 void print_tasks(*tasks){
     for (int i = 0; i < tasks.size(); i++){
         cout << tasks[i]
     }
 }
-//Read tasks from file
+//Read tasks from file +
 vector <string>read_tasks(){
     vector <string> tasks;
     string s;
@@ -28,26 +28,50 @@ vector <string>read_tasks(){
     };
     return tasks;
 }
-//First start of the app
+//First start of the app +
 int start_app(){
     int user_action = std();
     return user_action;
 }
-//Write tasks to txt file
+//Write tasks to txt file +
 void write(*tasks){
     ofstream file;
     file.open('tasks.txt');
     for (int i = 0; i < tasks.size(); i++){
         file << tasks[i] << endl;
     }
-}
+} +
 //Action::delete task from vector and save
-void delete_task(*tasks){}
+void delete_task(*tasks){
+    cout << "Please enter the number of the task, you want to delete\n";
+    int n;
+    cin >> n;
+    cout << "Task was deleted";
+    tasks.erase(taks.begin()+n);
+    write(tasks);
+} +
 //Action::add task to the list and save
-void add_task(*tasks){}
+void add_task(*tasks){
+    cout << "What task do you want to add?\n";
+    string task;
+    cin >> task;
+    cout << "Task was added";
+    tasks.push_back(task);
+    write(*tasks);
+} +
 //Action::change task from the list and save
-void change_tak(*tasks){}
-//Standart output: Available actions + To do list
+void change_tak(*tasks){
+    cout << "Please, enter the number of the task you want to delete\n";
+    int n;
+    cin >> n;
+    cout << "Please, enter the new value of that task\n";
+    string val;
+    cin >> val;
+    tasks.erase(taks.begin()+n);
+    tasks.insert(taks.begin()+n, val);
+    write(tasks);
+} +
+//Standart output: Available actions + To do list +
 int std(){
     user_menu();
     vector <string> tasks = read_tasks();
@@ -70,7 +94,7 @@ int std(){
             break;
     }
 }
-//Main function
+//Main function +
 int main() {
     int user_action = start_app();
 
