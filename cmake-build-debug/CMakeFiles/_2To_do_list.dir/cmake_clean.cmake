2e15d109ba3331e35550d4c9f91c7ed726d5b9be
Makar Baderko <makarbaderko@gmail.com>
2020-08-07 19:09:43 +0300

    Iniyialized clion project

---
+++ cmake-build-debug/CMakeFiles/_2To_do_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_2To_do_list.dir/main.cpp.o"
+  "_2To_do_list"
+  "_2To_do_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_2To_do_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
