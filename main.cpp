f6e2c8d676e0db82f43e2877a080426c63e880a9
Makar Baderko <makarbaderko@gmail.com>
2020-08-07 19:37:54 +0300

    Added such functions as: read_tasks, print_tasks, print_basic_menu;

--- main.cpp
+++ main.cpp
@@ -1,11 +1,60 @@
 #include <iostream>
 #include <string.h>
+#include <fstream>
+#include <vector>
 
 using namespace std;
+
 void user_menu(){
     string menu = "Подсказки: введите\nADD - чтобы добавить задачу\nDEL - чтобы удалить задачу\nCHG - чтобы изменить задачу";
     cout << menu;
 }
-int main() {
+
+void print_tasks(*tasks){
+    for (int i = 0; i < tasks.size(); i++){
+        cout << tasks[i]
+    }
+}
+vector read_tasks(){
+    vector <string> tasks;
+    string s;
+    ifstream file;
+    file.open('tasks.txt');
+    while(!file.eof()){
+        getline(file, s, '\n');
+        tasks.push_back(s);
+    };
+    return tasks;
+}
+vector start_app(){
     user_menu();
+    vector <string> tasks = read_tasks();
+    print_tasks(*tasks);
+    return tasks;
+}
+
+int std(){
+    user_menu();
+    vector <string> tasks = read_tasks();
+    print_tasks(*tasks);
+    cout << "Choose on of actions listed above, please\n";
+    string action;
+    cin >> action;
+    switch(action){
+        case "ADD":
+            return 1;
+            break;
+        case "DEL":
+            return 2;
+            break;
+        case "CHG":
+            return 3;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+int main() {
+    start_app();
 }
